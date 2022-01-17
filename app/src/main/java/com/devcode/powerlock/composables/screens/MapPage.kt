811fc7c52ce138177231d55ba9b2cebf57f2ff12
alexgmp6uni <alexgmp6uni@gmail.com>
2022-01-17 21:46:34 +0100

    error in map location point

--- app/src/main/java/com/devcode/powerlock/composables/screens/MapPage.kt
+++ app/src/main/java/com/devcode/powerlock/composables/screens/MapPage.kt
@@ -1,10 +1,45 @@
 package com.devcode.powerlock.composables.screens
 
+import android.os.Bundle
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import androidx.navigation.NavController
 import com.devcode.powerlock.composables.MapToolBar
+import com.devcode.powerlock.model.getCurrentUserName
+import com.devcode.powerlock.model.getLatLong
+import com.google.android.libraries.maps.CameraUpdateFactory
+import com.google.android.libraries.maps.MapView
+import com.google.android.libraries.maps.model.LatLng
+import com.google.android.libraries.maps.model.MarkerOptions
+import com.google.maps.android.ktx.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @Composable
 fun MapPage(navController: NavController) {
@@ -19,5 +54,130 @@ fun MapPage(navController: NavController) {
 @Preview(name="map content")
 @Composable
 fun MapContent() {
+GoogleMaps()
     
 }
+@Preview(name = "googlemaps")
+@Composable
+fun GoogleMaps() {
+
+    var mapView = rememberMapViewWithLifeCycle()
+    var locations= getLatLong(getCurrentUserName(LocalContext.current).toString())
+
+    var latlng= remember {   mutableStateOf("lat : 17.385,lang: 78.4867") }
+    Box(){
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+        ) {
+            AndroidView(
+                {mapView}
+            ) { mapView ->
+                CoroutineScope(Dispatchers.Main).launch {
+
+
+                    mapView.getMapAsync {
+
+                        it.mapType=1
+                        it.uiSettings.isZoomControlsEnabled = true
+                        val mark1 = LatLng(locations.get("latitud")!!.toDouble(), locations.get("longitud")!!.toDouble()) //Hyderabad
+
+
+                        it.moveCamera(CameraUpdateFactory.newLatLngZoom(mark1, 12f))
+                        it.setOnCameraIdleListener {
+                            it.clear()
+                            it.addMarker(
+                                MarkerOptions()
+                                .position(it.getCameraPosition().target))
+                            latlng.value=("lat: ${it.getCameraPosition().target.latitude}," +
+                                    " \n lang: ${it.getCameraPosition().target.longitude}")
+                        }
+                        val markerOptions =  MarkerOptions()
+                            .title("Hyderabad")
+                            .position(mark1)
+                        it.addMarker(markerOptions)
+
+
+
+
+                    }
+
+
+
+
+                }
+            }
+        }
+
+        Card (modifier = Modifier
+            .background(Color.Transparent)
+            .padding(12.dp)
+            ,elevation = 10.dp
+        ){
+            OutlinedTextField( value =latlng.value,
+
+                onValueChange = {
+
+                },
+                textStyle = TextStyle(
+                    color = Color.Red,
+
+                    fontFamily = FontFamily.SansSerif
+                ),
+                modifier = Modifier.background(Color.Transparent)
+                , leadingIcon = {
+                    // In this method we are specifying
+                    // our leading icon and its color.
+                    Icon(
+                        imageVector = Icons.Default.LocationOn,
+                        contentDescription = "image",
+                        tint = Color.Green
+                    )
+                },
+            )
+
+        }
+
+
+    }
+
+}
+
+@Preview(name = "rememberMapView")
+@Composable
+fun rememberMapViewWithLifeCycle(): MapView {
+    val context = LocalContext.current
+    val mapView = remember {
+        MapView(context).apply {
+            id = R.id.map_frame
+        }
+    }
+    val lifeCycleObserver = rememberMapLifecycleObserver(mapView)
+    val lifeCycle = LocalLifecycleOwner.current.lifecycle
+    DisposableEffect(lifeCycle) {
+        lifeCycle.addObserver(lifeCycleObserver)
+        onDispose {
+            lifeCycle.removeObserver(lifeCycleObserver)
+        }
+    }
+
+    return mapView
+}
+
+@Composable
+fun rememberMapLifecycleObserver(mapView: MapView): LifecycleEventObserver =
+    remember(mapView) {
+        LifecycleEventObserver { _, event ->
+            when(event) {
+                Lifecycle.Event.ON_CREATE -> mapView.onCreate(Bundle())
+                Lifecycle.Event.ON_START -> mapView.onStart()
+                Lifecycle.Event.ON_RESUME -> mapView.onResume()
+                Lifecycle.Event.ON_PAUSE -> mapView.onPause()
+                Lifecycle.Event.ON_STOP -> mapView.onStop()
+                Lifecycle.Event.ON_DESTROY -> mapView.onDestroy()
+                else -> throw IllegalStateException()
+            }
+        }
+    }
---
+++ app/src/main/java/com/devcode/powerlock/model/Firebase.kt
@@ -0,0 +1,113 @@
+package com.devcode.powerlock.model
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.SharedPreferences
+import android.location.Location
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.orhanobut.logger.Logger
+
+@SuppressLint("StaticFieldLeak")
+private var db = Firebase.firestore
+
+fun saveLocation(location : Location, androidID : String) {
+	val locationList = hashMapOf(
+		"longitud" to "${location.latitude}",
+		"latitud" to "${location.longitude}"
+	)
+	db.collection("phones").document(androidID).set(locationList, SetOptions.merge())
+
+}
+
+fun setGPSLocationState(state : Boolean, androidID : String) {
+	val state = hashMapOf(
+		"GPSLocationState" to state
+	)
+	db.collection("phones").document(androidID).set(state, SetOptions.merge())
+
+}
+
+fun getGPSLocationStateToSharedPreferences(
+	androidID : String?,
+	sharedPreferences : SharedPreferences
+) {
+	// Create a reference to the document wanted
+	val ed : SharedPreferences.Editor = sharedPreferences.edit()
+	val docRef = db.collection("phones").document(androidID!!)
+	// Create a query against the collection.
+	var state : Boolean? = false
+	Logger.d("step before docRef.get")
+	docRef.get()
+		.addOnSuccessListener { document ->
+			if (document.exists()) {
+				state = document.getBoolean("GPSLocationState")
+				Logger.d("document exist getGPSLocationState value --> $state")
+				ed.putBoolean("GPS", state!!)
+				ed.apply()
+
+			} else {
+				Logger.e("document does not exists")
+			}
+		}
+		.addOnFailureListener {
+			Logger.e("failure getGPSLocatonState--> ${it.toString()}")
+		}
+
+}
+
+fun getLatLong(userName : String) : HashMap<String?, String?> {
+	var location : HashMap<String?, String?> = hashMapOf()
+
+	db.collection("phones").whereEqualTo("user", userName).get()
+		.addOnSuccessListener { documents ->
+			for (document in documents) {
+				Logger.d("${document.id}=>${document.data}")
+				db.collection("phones").document(document.id).get()
+					.addOnSuccessListener { documentSnapShot ->
+						if (documentSnapShot.exists()) {
+							var latitud =
+								location.put("latitud", documentSnapShot.getString("latitud"))
+							Logger.d("latitud >>> $latitud")
+							var longitud =
+								location.put("longitud", documentSnapShot.getString("longitud"))
+							Logger.d("latitud >>> $longitud")
+						}
+					}
+			}
+		}
+
+
+	return location
+}
+
+fun getCurrentUserName(context : Context) : String? {
+	val user = Firebase.auth.currentUser
+	user?.let {
+		// Name, email address, and profile photo Url
+		//val name = user.displayName
+		//val photoUrl = user.photoUrl
+
+		val email = user.email
+
+		// Check if user's email is verified
+		//val emailVerified = user.isEmailVerified
+
+		// The user's ID, unique to the Firebase project. Do NOT use this value to
+		// authenticate with your backend server, if you have one. Use
+		// FirebaseUser.getToken() instead.
+		//val uid = user.uid
+	}
+	if (user != null) {
+		return user.email.toString()
+	}
+	return null
+}
+
+fun getDb() : FirebaseFirestore {
+	return Firebase.firestore
+
+}
