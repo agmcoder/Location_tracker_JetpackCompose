804627c9484dc14a078e1244b3a41fa9c6652495
agmcoder <agmcoder@gmail.com>
2022-02-24 01:04:29 +0100

    focus and lazy column stable in login screen

--- app/src/main/java/com/devcode/powerlock/view/screens/login/LoginScreen.kt
+++ app/src/main/java/com/devcode/powerlock/view/screens/login/LoginScreen.kt
@@ -1,26 +1,24 @@
 package com.devcode.powerlock.view.screens.login
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.SharedPreferences
 import android.widget.Toast
 import androidx.compose.foundation.*
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
@@ -29,6 +27,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
@@ -49,252 +48,301 @@ import kotlinx.coroutines.launch
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun LoginPage(
-    navController: NavController,
-    sharedPreferences: SharedPreferences
+	navController : NavController,
+	sharedPreferences : SharedPreferences
 ) {
-    val coroutineScope = rememberCoroutineScope()
-    val ed = sharedPreferences.edit()
-    val checkBoxState = rememberSaveable { mutableStateOf(false) }
-    val (emailRequest, passwordRequest) = FocusRequester.createRefs()
-    val context = LocalContext.current
-    val image = painterResource(R.drawable.logo2)
-    val emailValue = rememberSaveable { mutableStateOf("") }
-    val passwordValue = rememberSaveable { mutableStateOf("") }
-    val passwordVisibility = remember { mutableStateOf(false) }
-    val scrollState = rememberScrollState()
-    val focusManager = LocalFocusManager.current
+	val coroutineScope = rememberCoroutineScope()
+	val ed = sharedPreferences.edit()
+	val checkBoxState = rememberSaveable { mutableStateOf(false) }
+	val (emailRequest, passwordRequest) = FocusRequester.createRefs()
+	val context = LocalContext.current
+	val image = painterResource(R.drawable.logo2)
+	val emailValue = rememberSaveable { mutableStateOf("") }
+	val passwordValue = rememberSaveable { mutableStateOf("") }
+	val passwordVisibility = remember { mutableStateOf(false) }
+	val scrollState = rememberScrollState()
+	val focusManager = LocalFocusManager.current
+	val focusRequester = FocusRequester()
 
-    //we start to implement login screen UI-->
+	//we start to implement login screen UI-->
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colors.background)
-            .clickable { focusManager.clearFocus() }
-            .pointerInput(Unit) {
-                detectTapGestures(
-                    onTap = {
-                        focusManager.clearFocus()
-                    }
-                )
+	Column(
+		modifier = Modifier
+			.fillMaxSize()
+			.background(MaterialTheme.colors.background)
+			.clickable { focusManager.clearFocus() }
+			.pointerInput(Unit) {
+				detectTapGestures(
+					onTap = {
+						focusManager.clearFocus()
+					}
+				)
+
+			},
+		horizontalAlignment = Alignment.CenterHorizontally
+	) {
+
+		Box(//this is the logo box
+			modifier = Modifier
+				.fillMaxHeight(0.3f)
+				.background(MaterialTheme.colors.background),
+			contentAlignment = Alignment.Center
+
+		) {
+
+			Image(
+				painter = image, contentDescription = "logo",
+				alignment = Alignment.Center,
+				modifier = Modifier.fillMaxSize()
+			)
+
+		}
+		LazyColumn(
+			verticalArrangement = Arrangement.Center,
+			horizontalAlignment = Alignment.CenterHorizontally
+		) {
+
+			item {
+				Text(
+					modifier = Modifier.padding(5.dp),
+					text = stringResource(id = R.string.signIn),
+					style = MaterialTheme.typography.h2,
+					color = MaterialTheme.colors.onPrimary
+
+				)
+			}
 
 
-            },
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
+			item {
 
-        Box(//this is the logo box
-            modifier = Modifier
-                .fillMaxHeight(0.3f)
-                .background(MaterialTheme.colors.background),
-            contentAlignment = Alignment.Center
-
-        ) {
-
-            Image(
-                painter = image, contentDescription = "logo",
-                alignment = Alignment.Center,
-                modifier = Modifier.fillMaxSize()
-            )
-
-        }
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier =
-            Modifier
-                .fillMaxSize()
-                .clip(RoundedCornerShape(topEnd = 30.dp, topStart = 30.dp))
-                .background(MaterialTheme.colors.surface)
-
-        ) {
-            LazyColumn(){
-
-
-            item{
-            Text(
-                modifier = Modifier.padding(5.dp),
-                text = stringResource(id = R.string.signIn),
-                style = MaterialTheme.typography.h2,
-                color = MaterialTheme.colors.onPrimary
-
-            )}
-    item{
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(10.dp)
-                    .verticalScroll(scrollState)
-
-            ) {
-
-                Spacer(modifier = Modifier.padding(20.dp))
+				Spacer(modifier = Modifier.padding(20.dp))
+			}
 
 
 
+			item {
+				OutlinedTextField(
 
-                Column(horizontalAlignment = Alignment.CenterHorizontally)
-                {
-                    OutlinedTextField(
+					value = emailValue.value,
+					keyboardOptions = KeyboardOptions(
+						imeAction = ImeAction.Next,
+						keyboardType = KeyboardType.Password
+					),
+					keyboardActions = KeyboardActions(
+					),
+					onValueChange = { emailValue.value = it },
 
-                        value = emailValue.value,
-                        keyboardOptions = KeyboardOptions(
-                            keyboardType = KeyboardType.Password
-                        ),
-                        keyboardActions = KeyboardActions(),
-                        onValueChange = { emailValue.value = it },
+					label = {
+						Text(
+							text = "user email",
+							color = MaterialTheme.colors.onPrimary
+						)
+					},
+					placeholder = {
+						Text(
+							text = "user email",
+							color = MaterialTheme.colors.secondaryVariant
+						)
+					},
+					singleLine = true,
+					modifier = Modifier
+						.fillMaxWidth(0.8f)
+						.focusRequester(focusRequester = focusRequester),
+					textStyle = TextStyle(color = MaterialTheme.colors.onPrimary)
 
-                        label = {
-                            Text(
-                                text = "user email",
-                                color = MaterialTheme.colors.onPrimary
-                            )
-                        },
-                        placeholder = {
-                            Text(
-                                text = "user email",
-                                color = MaterialTheme.colors.secondaryVariant
-                            )
-                        },
-                        singleLine = true,
-                        modifier = Modifier
-                            .fillMaxWidth(0.8f)
-                            .focusable(),
-                        textStyle = TextStyle(color = MaterialTheme.colors.onPrimary)
+				)
+			}
+			item {
+				OutlinedTextField(
 
-                    )
-                    OutlinedTextField(
+					value = passwordValue.value,
+					keyboardOptions = KeyboardOptions(
+						imeAction = ImeAction.Done,
+						keyboardType = KeyboardType.Password
+					),
+					keyboardActions = KeyboardActions(
+						onDone = {
+							focusManager.clearFocus()
+							coroutineScope.launch {
+								loginChecker(
+									emailValue,
+									passwordValue,
+									context,
+									ed
+								).collect {
+									when (it) {
+										LoginDeviceState.OBSERVER -> navController.navigate("map_page")
+										LoginDeviceState.EMITTER -> navController.navigate("menu_page")
+										LoginDeviceState.ERROR -> Toast.makeText(
+											context,
+											"Authentication failed.",
+											Toast.LENGTH_SHORT
+										).show()
+									}
+								}
 
-                        value = passwordValue.value,
-                        keyboardOptions = KeyboardOptions(
-                            keyboardType = KeyboardType.Password
-                        ),
-                        keyboardActions = KeyboardActions(),
-                        onValueChange = { passwordValue.value = it },
-                        trailingIcon = {
-                            IconButton(onClick = {
-                                passwordVisibility.value = !passwordVisibility.value
-                            }) {
+							}
 
-                                Icon(
-                                    imageVector = ImageVector.vectorResource(
-                                        id = R.drawable.password_eye
-                                    ),
-                                    contentDescription = "",
-                                    tint = if (passwordVisibility.value)
-                                        primaryColor else MaterialTheme.colors.onPrimary
+						}
+					),
+					onValueChange = { passwordValue.value = it },
+					trailingIcon = {
+						IconButton(onClick = {
+							passwordVisibility.value = !passwordVisibility.value
+						}) {
 
-                                )
+							Icon(
+								imageVector = ImageVector.vectorResource(
+									id = R.drawable.password_eye
+								),
+								contentDescription = "",
+								tint = if (passwordVisibility.value)
+									primaryColor else MaterialTheme.colors.onPrimary
 
-                            }
-                        },
-                        label = {
-                            Text(
-                                text = "Password",
-                                color = MaterialTheme.colors.onPrimary
-                            )
-                        },
-                        placeholder = {
-                            Text(
-                                text = "Password",
-                                color = MaterialTheme.colors.secondaryVariant
-                            )
-                        },
-                        singleLine = true,
-                        visualTransformation = if (passwordVisibility.value) VisualTransformation.None
-                        else PasswordVisualTransformation(),
-                        modifier = Modifier
-                            .fillMaxWidth(0.8f)
-                            .focusable(),
-                        textStyle = TextStyle(color = MaterialTheme.colors.onPrimary)
+							)
 
-                    )
+						}
+					},
+					label = {
+						Text(
+							text = "Password",
+							color = MaterialTheme.colors.onPrimary
+						)
+					},
+					placeholder = {
+						Text(
+							text = "Password",
+							color = MaterialTheme.colors.secondaryVariant
+						)
+					},
+					singleLine = true,
+					visualTransformation = if (passwordVisibility.value) VisualTransformation.None
+					else PasswordVisualTransformation(),
+					modifier = Modifier
+						.fillMaxWidth(0.8f)
+						.focusRequester(focusRequester),
+					textStyle = TextStyle(color = MaterialTheme.colors.onPrimary)
 
-                }
+				)
 
-
-
-                CustomSpacer()
-                Button(
-
-                    onClick = {
-                        coroutineScope.launch {
-                            loginChecker(
-                                emailValue,
-                                passwordValue,
-                                context,
-                                ed
-                            ).collect {
-                                when (it) {
-                                    LoginDeviceState.OBSERVER -> navController.navigate("map_page")
-                                    LoginDeviceState.EMITTER -> navController.navigate("menu_page")
-                                    LoginDeviceState.ERROR -> Toast.makeText(
-                                        context,
-                                        "Authentication failed.",
-                                        Toast.LENGTH_SHORT
-                                    ).show()
-                                }
-                            }
-
-                        }
-
-                    },
-                    modifier = Modifier
-                        .fillMaxWidth(0.8f)
-                        .height(50.dp)
-                        .clip(MaterialTheme.shapes.medium),
-                    colors = ButtonDefaults.buttonColors(
-                        contentColor = MaterialTheme.colors.primaryVariant
-                    )
-
-                ) {
-                    Text(text = "Sign In", fontSize = 20.sp)
-                }
+			}
 
 
-                CustomSpacer()
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center
-                ) {
-                    Text(
-                        text = "remember user and password?",
-                        color = MaterialTheme.colors.onPrimary
-                    )
-                    Checkbox(checked = checkBoxState.value,
-                        modifier = Modifier.border(
-                            width = 1.dp,
-                            color = MaterialTheme.colors.onPrimary
-                        ),
-                        onCheckedChange =
-                        {
-                            checkBoxState.value = it
+			item {
+				CustomSpacer()
+				Button(
 
-                        }
-                    )
+					onClick = {
+						coroutineScope.launch {
+							loginChecker(
+								emailValue,
+								passwordValue,
+								context,
+								ed
+							).collect {
+								when (it) {
+									LoginDeviceState.OBSERVER -> navController.navigate("map_page")
+									LoginDeviceState.EMITTER -> navController.navigate("menu_page")
+									LoginDeviceState.ERROR -> Toast.makeText(
+										context,
+										"Authentication failed.",
+										Toast.LENGTH_SHORT
+									).show()
+								}
+							}
 
-                }
-                CustomSpacer()
-                Text(
-                    text = "Create An Account",
-                    modifier = Modifier.clickable(onClick = {
-                        navController.navigate("register_page") {
-                            //popUpTo = navController.graph.startDestination
-                            //launchSingleTop = true
-                        }
-                    }
-                    ),
-                    color = MaterialTheme.colors.onPrimary
+						}
+					},
+					modifier = Modifier
+						.fillMaxWidth(0.8f)
+						.height(50.dp)
+						.clip(MaterialTheme.shapes.medium),
+					colors = ButtonDefaults.buttonColors(
+						contentColor = MaterialTheme.colors.primaryVariant
+					)
 
-                )
-                CustomSpacer()
+				) {
+					Text(text = "Sign In", fontSize = 20.sp)
+				}
+			}
 
-            }}
-        }
-    }
-    }
+			item {
+				CustomSpacer()
+				Row(
+					modifier = Modifier.fillMaxWidth(),
+					horizontalArrangement = Arrangement.Center
+				) {
+					Text(
+						text = "remember user and password?",
+						color = MaterialTheme.colors.onPrimary
+					)
+					Checkbox(checked = checkBoxState.value,
+						modifier = Modifier.border(
+							width = 1.dp,
+							color = MaterialTheme.colors.onPrimary
+						),
+						onCheckedChange =
+						{
+							checkBoxState.value = it
+
+						}
+					)
+
+				}
+				CustomSpacer()
+				Text(
+					text = "Create An Account",
+					modifier = Modifier.clickable(onClick = {
+						navController.navigate("register_page") {
+							//popUpTo = navController.graph.startDestination
+							//launchSingleTop = true
+						}
+					}
+					),
+					color = MaterialTheme.colors.onPrimary
+
+				)
+				Spacer(modifier = Modifier.height(200.dp))
+
+			}
+		}
+	}
 }
+//it is not used jet but i am working to use it an reuse code.
+@SuppressLint("CoroutineCreationDuringComposition")
+@Composable
+fun PressButton(
+	emailValue : MutableState<String>,
+	passwordValue : MutableState<String>,
+	context : Context,
+	ed : SharedPreferences.Editor,
+	navController : NavController
+) {
+	val coroutineScope = rememberCoroutineScope()
+	coroutineScope.launch {
+		loginChecker(
+			emailValue,
+			passwordValue,
+			context,
+			ed
+		).collect {
+			when (it) {
+				LoginDeviceState.OBSERVER -> navController.navigate("map_page")
+				LoginDeviceState.EMITTER -> navController.navigate("menu_page")
+				LoginDeviceState.ERROR -> Toast.makeText(
+					context,
+					"Authentication failed.",
+					Toast.LENGTH_SHORT
+				).show()
+			}
+		}
+
+	}
+
+}
+
+
+
 
 
 
